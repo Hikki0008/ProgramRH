803be543017e27bcecee360fe09142f4cc815aaa
Hikki0008 <hikki008hachi@gmail.com>
2025-03-23 17:04:37 -0300

    Add files via upload

---
+++ src/main/java/ProgramRh/Dto/CurriculoDto.java
@@ -0,0 +1,17 @@
+package ProgramRh.domain.Curriculo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CurriculoDTO {
+
+    private String nome;
+    private String email;
+    private String experiencia;
+    private String telefone;
+    private String endereco;
+    private String fileName;
+    private String contentType;
+}
